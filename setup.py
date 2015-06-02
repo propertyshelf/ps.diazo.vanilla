76e1cc0214d66476b5d5675b1a742d817f86fa38
Jens Krause <jens@propertyshelf.com>
2015-06-02 18:33:53 +0700

    prepare 0.8.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.8'
+version = '0.8.1'
 description = 'Propertyshelf Vanilla Theme.'
 long_description = ('\n'.join([
     open('README.rst').read(),
