64751ed7c175a2d5b040a099702278978137b988
Jens Krause <jens@propertyshelf.com>
2015-07-07 15:36:32 +0700

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '1.1'
+version = '1.2.dev0'
 description = 'Diazo Vanilla-Theme.'
 long_description = ('\n'.join([
     open('README.rst').read(),
