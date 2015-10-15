a2605459602db142ef12819b15c588fad26d71e2
Kantanart <kantanat@propertyshelf.com>
2015-10-15 14:13:34 +0700

    Improve Footer
    - Doormat column should work without javascript
    - Fixed about rules of footer
    - Include portal-footer
    - Include portal-siteactions
    - Can Re-order
    - Delete css about colophon

--- src/ps/diazo/vanilla/diazo_resources/js/support.js
+++ src/ps/diazo/vanilla/diazo_resources/js/support.js
@@ -378,38 +378,6 @@ function improve_site_social() {
         $(".site-social-switch a").toggleClass("active");
      });
 }
-
-/*get the existing doormat columns and translate them to the span grid system*/
-function getDoormatClass(){
-    var col_class="";
-    if($('.doormatColumn.column-0').length>0){
-        //just one column
-        col_class="col-md-12";
-    }
-    if($('.doormatColumn.column-1').length>0){
-        //two columns
-        col_class="col-md-6";
-    }
-    if($('.doormatColumn.column-2').length>0){
-        //three columns
-        col_class="col-md-4";
-    }
-    if($('.doormatColumn.column-3').length>0){
-        //four columns
-        col_class="col-md-3";
-    }
-    if($('.doormatColumn.column-4').length>0){
-        //five columns
-        col_class="";
-    }
-    if($('.doormatColumn.column-5').length>0){
-        //six columns
-        col_class="col-md-2";
-    }
-
-    return col_class;
-}
-
 /*Switch Cover page - Listing search */
 function toggle_listing_type(){
     $(".listing-search-tile #formfield-form-widgets-jacuzzi span").hide('slow');
@@ -561,11 +529,6 @@ $(document).ready(function() {
     if($("div.carousel").length >0){
         move_menubar();
     }
-    if($(".doormatColumn").length >0){
-        doormat_col_class=getDoormatClass();
-        $("#footer-top-inner .doormatColumn").addClass(doormat_col_class);
-    }
-
     // Ajax Complete
     $( document ).ajaxComplete(function() {
         if (is_ListingRowPage()) {
